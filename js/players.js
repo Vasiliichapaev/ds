3cf31c7100bda5ae6bffaf86384a458fd951bac9
Василий Чапаев <chapaevpustota@gmail.com>
2020-02-17 18:11:44 +0300

    refactoring

---
+++ js/players.js
@@ -0,0 +1,57 @@
+const players_data = [
+    {
+        name: "VChapaev",
+        accounts: [
+            {
+                id: 117990545
+            },
+            {
+                id: 254920273
+            }
+        ]
+    },
+    {
+        name: "Neeeeeerf",
+        accounts: [
+            {
+                id: 102756891
+            }
+        ]
+    },
+    {
+        name: "JohnGalt",
+        accounts: [
+            {
+                id: 41528404
+            }
+        ]
+    },
+    {
+        name: "Megabit",
+        accounts: [
+            {
+                id: 84502939
+            }
+        ]
+    },
+    {
+        name: "Alexfov",
+        accounts: [
+            {
+                id: 313885294
+            },
+            {
+                id: 153901833,
+                start_date: [14, 12, 2019]
+            }
+        ]
+    },
+    {
+        name: "BloOdTerrOr",
+        accounts: [
+            {
+                id: 120491980
+            }
+        ]
+    }
+];

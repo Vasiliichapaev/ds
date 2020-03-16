5df16891175e556f5e9b583088bcc18fbe63248c
Vasilii <chapaevpustota@gmail.com>
2020-03-16 20:56:40 +0300

    loading on

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 let main = new Main(players_data);
 
 main.load_data()
-    .then(a => {
+    .then(() => {
         main.calculation();
-//        main.display();
+        main.display();
     })
-//    .catch(err => {
-//        main.error_load_data();
-//        console.log(err);
-//    });
+    .catch(err => {
+        main.error_load_data();
+        console.log(err);
+    });
